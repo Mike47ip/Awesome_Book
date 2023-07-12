a5d91b80732c5e91eb0be53da62fc360d392d937
Bett Enock <bettenock968@gmail.com>
2023-07-12 12:55:40 -0700

    Add navigable Links to main page

--- index.js
+++ index.js
@@ -65,3 +65,49 @@ const addBookForm = document.getElementById('addBookForm');
 addBookForm.addEventListener('submit', (event) => bookCollection.addBook(event));
 
 bookCollection.displayBooks();
+
+// Displaying the navitems individually
+
+const mainLink = document.getElementById ('Main-Link');
+const addLink = document.getElementById ('Add-Page-Link');
+const contactLink = document.getElementById ('Contact_Link');
+
+
+const awesomeBooks = document.querySelector('.books-content-top');
+const contactPage = document.querySelector('.contact-container');
+const formPage = document.querySelector ('.form-container')
+
+mainLink.addEventListener ('click', ()=>{
+  mainLink.classList.toggle('clicked');
+  contactLink.classList.remove('clicked3');
+  addLink.classList.remove('clicked2');
+  formPage.classList.remove('less');
+  formPage.classList.remove('remove');
+  
+  contactPage.classList.remove('add');
+  awesomeBooks.classList.remove('more');
+
+
+  
+})
+
+addLink.addEventListener ('click', ()=>{
+  addLink.classList.toggle('clicked2');
+  mainLink.classList.remove('clicked');
+  contactLink.classList.remove('clicked3');
+  awesomeBooks.classList.add('more');
+  contactPage.classList.remove('add');
+  formPage.classList.add('less');
+  
+});
+
+contactLink.addEventListener ('click', ()=>{
+  addLink.classList.remove('clicked2');
+  contactLink.classList.toggle('clicked3');
+  mainLink.classList.remove('clicked');
+  awesomeBooks.classList.remove('more');
+  contactPage.classList.add('add');
+  formPage.classList.add('remove');
+  
+});
+
