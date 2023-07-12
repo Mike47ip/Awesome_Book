adaca20b19989f66d6d05bc2b9f2bdad41514225
Mike Dark <agyemankojo47@gmail.com>
2023-07-12 19:46:33 +0000

    Change navlinks

--- index.js
+++ index.js
@@ -70,46 +70,39 @@ bookCollection.displayBooks();
 
 // Displaying the navitems individually
 
-const mainLink = document.getElementById ('Main-Link');
-const addLink = document.getElementById ('Add-Page-Link');
-const contactLink = document.getElementById ('Contact_Link');
-
+const mainLink = document.getElementById('Main-Link');
+const addLink = document.getElementById('Add-Page-Link');
+const contactLink = document.getElementById('Contact_Link');
 
 const awesomeBooks = document.querySelector('.books-content-top');
 const contactPage = document.querySelector('.contact-container');
-const formPage = document.querySelector ('.form-container')
+const formPage = document.querySelector('.form-container');
 
-mainLink.addEventListener ('click', ()=>{
+mainLink.addEventListener('click', () => {
   mainLink.classList.toggle('clicked');
   contactLink.classList.remove('clicked3');
   addLink.classList.remove('clicked2');
-  formPage.classList.remove('less');
+  formPage.classList.add('less');
   formPage.classList.remove('remove');
-  
+
   contactPage.classList.remove('add');
-  awesomeBooks.classList.remove('more');
+  awesomeBooks.classList.add('more');
+});
 
-
-  
-})
-
-addLink.addEventListener ('click', ()=>{
+addLink.addEventListener('click', () => {
   addLink.classList.toggle('clicked2');
   mainLink.classList.remove('clicked');
   contactLink.classList.remove('clicked3');
-  awesomeBooks.classList.add('more');
+  awesomeBooks.classList.remove('more');
   contactPage.classList.remove('add');
-  formPage.classList.add('less');
-  
+  formPage.classList.remove('less');
 });
 
-contactLink.addEventListener ('click', ()=>{
+contactLink.addEventListener('click', () => {
   addLink.classList.remove('clicked2');
   contactLink.classList.toggle('clicked3');
   mainLink.classList.remove('clicked');
   awesomeBooks.classList.remove('more');
   contactPage.classList.add('add');
   formPage.classList.add('remove');
-  
 });
-
